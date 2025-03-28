b4912234c982d2c5919349ce65b50735aaf311d8
skyaz <az.skyes@outlook.com>
2025-03-28 16:45:42 +0000

    2nd commit - redundant test code removed from login_test.spec.js

--- tests/login_test.spec.js
+++ tests/login_test.spec.js
@@ -9,17 +9,6 @@ test('test', async ({ page }) => {
     const Login = new LoginPage(page)
 
     await Login.goToLoginPage()
-    await Login.login('student', 'Passwrod123')
+    await Login.login('student', 'Password123')
 
-
-
-    // await page.goto('https://practicetestautomation.com/practice-test-login/');
-    // await page.getByRole('textbox', { name: 'Username' }).click();
-    // await page.getByRole('textbox', { name: 'Username' }).fill('student');
-    // await page.getByRole('textbox', { name: 'Username' }).press('Enter');
-    // await page.getByRole('textbox', { name: 'Username' }).press('Tab');
-    // await page.getByRole('textbox', { name: 'Password' }).click();
-    // await page.getByRole('textbox', { name: 'Password' }).fill('Password123');
-    // await page.getByRole('button', { name: 'Submit' }).click();
-    // await page.getByRole('link', { name: 'Log out' }).click();
 });
